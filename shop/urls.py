4aef213e7dc3723f7af095c08278564344ead776
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-04-25 10:03:31 +0530

    module-35

--- shop/urls.py
+++ shop/urls.py
@@ -1,20 +1,38 @@
 
 from django.urls import path
 
-from shop import views
+from shop.views import (
+    HomeView,
+    SearchView,
+    CartView,
+    MyCartView,
+    CheckoutView,
+    DetailView,
+    CategoriesView,
+    ApiProductsView,
+    SignupView,
+    SigninView,
+    SignoutView
+    
+)
 
 app_name = "shop"
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('signup/', views.signup, name="signup"),
-    path('signin/', views.signin, name="signin"),
-    path('signout/', views.signout, name="signout"),
-    path('search/', views.search, name="search"),
-    path('<slug>/cart/', views.cart, name="cart"),
-    path('mycart/', views.mycart, name="mycart"),
-    path('checkout/', views.checkout, name="checkout"),
-    path('<slug>/', views.detail, name="detail"),
-    path('categories/<slug>/', views.categories, name="categories"),
-    path('api/products/', views.api_products, name="api_products"),
+    path('', HomeView.as_view(), name="home"),
+    path('signup/', SignupView.as_view(), name="signup"),
+    path('signin/', SigninView.as_view(), name="signin"),
+    path('signout/', SignoutView.as_view(), name="signout"),
+    path('search/', SearchView.as_view(), name="search"),
+    path('<slug>/cart/', CartView.as_view(), name="cart"),
+    path('mycart/', MyCartView.as_view(), name="mycart"),
+    path('checkout/', CheckoutView.as_view(), name="checkout"),
+    path('<slug>/', DetailView.as_view(), name="detail"),
+    path('categories/<slug>/', CategoriesView.as_view(), name="categories"),
+    path('api/products/', ApiProductsView.as_view(), name="api_products"),
 ]
+
+
+
+    
+   
