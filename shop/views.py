9257de2aed8e733bf2f558c6392a3a1841ed9222
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-04-25 09:53:45 +0530

    module-34

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,9 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.views import View
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -13,34 +12,46 @@ from shop.models import Product, Category
 from shop.serializer import ProductSerializer
 
 
-def home(request):
-    products = Product.objects.filter(active=True)
-    categories = Category.objects.filter(active=True)
-    context = {"products": products, "categories": categories}
-    return render(request, "shop/home.html", context)
+class HomeView(View):
+    def get(self, request):
+        products = Product.objects.filter(active=True)
+        categories = Category.objects.filter(active=True)
+        context = {"products": products, "categories": categories}
+        return render(request, "shop/home.html", context)
 
 
-def search(request):
-    q = request.GET["q"]
-    products = Product.objects.filter(active=True, name__icontains=q)
-    categories = Category.objects.filter(active=True)
-    context = {"products": products,
-               "categories": categories,
-               "title": q + " - search"}
-    return render(request, "shop/list.html", context)
+class SearchView(View):
+    def get(self, request):
+        q = request.GET["q"]
+        products = Product.objects.filter(active=True, name__icontains=q)
+        categories = Category.objects.filter(active=True)
+        context = {"products": products,
+                   "categories": categories,
+                   "title": q + " - search"}
+        return render(request, "shop/list.html", context)
 
 
-def categories(request, slug):
-    cat = Category.objects.get(slug=slug)
-    products = Product.objects.filter(active=True, category=cat)
-    categories = Category.objects.filter(active=True)
-    context = {"products":products, "categories":categories, "title":cat.name + " - Categories"}
-    return render(request, "shop/list.html", context)
+class CategoriesView(View):
+    def get(self, request, slug):
+        cat = Category.objects.get(slug=slug)
+        products = Product.objects.filter(active=True, category=cat)
+        categories = Category.objects.filter(active=True)
+        context = {"products": products, "categories": categories, "title": cat.name + " - Categories"}
+        return render(request, "shop/list.html", context)
 
 
-def detail(request, slug):
-    product = Product.objects.get(active=True, slug=slug)
-    if request.method == "POST":
+class DetailView(View):
+    def get(self, request, slug):
+        product = Product.objects.get(active=True, slug=slug)
+        form = ReviewForm()
+        categories = Category.objects.filter(active=True)
+        context = {"product": product,
+                   "categories": categories,
+                   "form": form}
+        return render(request, "shop/detail.html", context)
+
+    def post(self, request, slug):
+        product = Product.objects.get(active=True, slug=slug)
         form = ReviewForm(request.POST)
         if form.is_valid():
             review = form.save(commit=False)
@@ -50,57 +61,50 @@ def detail(request, slug):
             messages.success(request, "Review saved")
         else:
             messages.error(request, "Invalid form")
-    else:
-        form = ReviewForm()
+        categories = Category.objects.filter(active=True)
+        context = {"product": product,
+                   "categories": categories,
+                   "form": form}
+        return render(request, "shop/detail.html", context)
 
 
-    categories = Category.objects.filter(active=True)
-    context = {"product" : product,
-               "categories":categories,
-               "form": form}
-    return render(request, "shop/detail.html", context)
+class CartView(View):
+    def get(self, request, slug):
+        product = Product.objects.get(slug=slug)
+        inital = {"items":[],"price":0.0,"count":0}
+        session = request.session.get("data", inital)
+        if slug in session["items"]:
+            messages.error(request, "Already added to cart")
+        else:
+            session["items"].append(slug)
+            session["price"] += float(product.price)
+            session["count"] += 1
+            request.session["data"] = session
+            messages.success(request, "Added successfully")
+        return redirect("shop:detail", slug)
+
+
+class MyCartView(View):
+    def get(self, request):
+        sess = request.session.get("data", {"items":[]})
+        products = Product.objects.filter(active=True, slug__in=sess["items"])
+        categories = Category.objects.filter(active=True)
+        context = {"products": products,
+                   "categories": categories,
+                   "title": "My Cart"}
+        return render(request, "shop/list.html", context)
+
+
+class CheckoutView(View):
+    def get(self, request):
+        request.session.pop('data', None)
+        return redirect("/")
 
 
 
-def cart(request, slug):
-    """
-        data = {"items" : ["slug1", "slug2"],
-                "price" : 12342,
-                "count" : 5
-                }
-        request.session["data"] = data
-        """
-    product = Product.objects.get(slug=slug)
-    inital = {"items":[],"price":0.0,"count":0}
-    session = request.session.get("data", inital)
-    if slug in session["items"]:
-        messages.error(request, "Already added to cart")
-    else:
-        session["items"].append(slug)
-        session["price"] += float(product.price)
-        session["count"] += 1
-        request.session["data"] = session
-        messages.success(request, "Added successfully")
-    return redirect("shop:detail", slug)
-
-
-def mycart(request):
-    sess = request.session.get("data", {"items":[]})
-    products = Product.objects.filter(active=True, slug__in=sess["items"])
-    categories = Category.objects.filter(active=True)
-    context = {"products": products,
-               "categories": categories,
-               "title": "My Cart"}
-    return render(request, "shop/list.html", context)
-
-
-def checkout(request):
-    request.session.pop('data', None)
-    return redirect("/")
-
-@api_view(['GET'])
-def api_products(request):
-    query = request.GET.get("q", "")
-    products = Product.objects.filter(Q(name__contains=query) | Q(description__contains=query))
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+class ApiProductsView(APIView):
+    def get(self, request):
+        query = request.GET.get("q", "")
+        products = Product.objects.filter(Q(name__contains=query) | Q(description__contains=query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
