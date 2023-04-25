f92fefefb86824b0b8d0bf2fa5675b6a7a2bc734
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-04-25 09:48:37 +0530

    module-33

--- shop/views.py
+++ shop/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from shop.forms import ReviewForm, SignupForm, SigninForm
@@ -13,17 +13,17 @@ from shop.models import Product, Category
 from shop.serializer import ProductSerializer
 
 
-def home(request):
-    products = Product.objects.filter(active=True)
-    categories = Category.objects.filter(active=True)
-    context = {"products": products, "categories": categories}
-    return render(request, "shop/home.html", context)
+class HomeView(View):
+    def get(self, request):
+        products = Product.objects.filter(active=True)
+        categories = Category.objects.filter(active=True)
+        context = {"products": products, "categories": categories}
+        return render(request, "shop/home.html", context)
 
 
-
-@api_view(['GET'])
-def api_products(request):
-    query = request.GET.get("q", "")
-    products = Product.objects.filter(Q(name__contains=query) | Q(description__contains=query))
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+class ApiProductsView(APIView):
+    def get(self, request):
+        query = request.GET.get("q", "")
+        products = Product.objects.filter(Q(name__contains=query) | Q(description__contains=query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
