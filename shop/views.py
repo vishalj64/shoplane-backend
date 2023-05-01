aaa6191248bd5b84cb70038412dc9c34d7b05604
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-05-01 16:13:33 +0530

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,24 +1,19 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.db.models import Q
-from django.shortcuts import render, redirect
-from django.views import View
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
-from shop.forms import ReviewForm, SignupForm, SigninForm
-from shop.models import Product, Category
-from shop.serializer import ProductSerializer
-
-
-class HomeView(View):
+class HomeView(APIView):
     def get(self, request):
         products = Product.objects.filter(active=True)
         categories = Category.objects.filter(active=True)
-        context = {"products": products, "categories": categories}
-        return render(request, "shop/home.html", context)
+        serializer = ProductSerializer(products, many=True)
+        data = {
+            "products": serializer.data,
+            "categories": [cat.name for cat in categories]
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+
 
 
 class ApiProductsView(APIView):
@@ -26,4 +21,5 @@ class ApiProductsView(APIView):
         query = request.GET.get("q", "")
         products = Product.objects.filter(Q(name__contains=query) | Q(description__contains=query))
         serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
