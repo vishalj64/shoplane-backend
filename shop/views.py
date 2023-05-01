61bd24e8635bf116fc0e8bf07f4b860e7f29cb70
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-05-01 16:12:48 +0530

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,54 +1,40 @@
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
-from django.db.models import Q
-from django.shortcuts import render, redirect
-from django.views import View
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
-from shop.forms import ReviewForm, SignupForm, SigninForm
-from shop.models import Product, Category
-from shop.serializer import ProductSerializer
-
-
-class HomeView(View):
+class HomeView(APIView):
     def get(self, request):
         products = Product.objects.filter(active=True)
         categories = Category.objects.filter(active=True)
-        context = {"products": products, "categories": categories}
-        return render(request, "shop/home.html", context)
+        serializer = ProductSerializer(products, many=True)
+        data = {
+            "products": serializer.data,
+            "categories": [cat.name for cat in categories]
+        }
+        return Response(data, status=status.HTTP_200_OK)
 
-
-class SearchView(View):
+class SearchView(APIView):
     def get(self, request):
         q = request.GET["q"]
         products = Product.objects.filter(active=True, name__icontains=q)
-        categories = Category.objects.filter(active=True)
-        context = {"products": products,
-                   "categories": categories,
-                   "title": q + " - search"}
-        return render(request, "shop/list.html", context)
+        serializer = ProductSerializer(products, many=True)
+        data = {"products": serializer.data}
+        return Response(data, status=status.HTTP_200_OK)
 
-
-class CategoriesView(View):
+class CategoriesView(APIView):
     def get(self, request, slug):
         cat = Category.objects.get(slug=slug)
         products = Product.objects.filter(active=True, category=cat)
-        categories = Category.objects.filter(active=True)
-        context = {"products": products, "categories": categories, "title": cat.name + " - Categories"}
-        return render(request, "shop/list.html", context)
+        serializer = ProductSerializer(products, many=True)
+        data = {"products": serializer.data}
+        return Response(data, status=status.HTTP_200_OK)
 
-
-class DetailView(View):
+class DetailView(APIView):
     def get(self, request, slug):
         product = Product.objects.get(active=True, slug=slug)
-        form = ReviewForm()
-        categories = Category.objects.filter(active=True)
-        context = {"product": product,
-                   "categories": categories,
-                   "form": form}
-        return render(request, "shop/detail.html", context)
+        serializer = ProductSerializer(product)
+        data = {"product": serializer.data}
+        return Response(data, status=status.HTTP_200_OK)
 
     def post(self, request, slug):
         product = Product.objects.get(active=True, slug=slug)
@@ -59,52 +45,41 @@ class DetailView(View):
             review.user = request.user
             review.save()
             messages.success(request, "Review saved")
+            return Response(status=status.HTTP_201_CREATED)
         else:
-            messages.error(request, "Invalid form")
-        categories = Category.objects.filter(active=True)
-        context = {"product": product,
-                   "categories": categories,
-                   "form": form}
-        return render(request, "shop/detail.html", context)
+            return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class CartView(View):
+class CartView(APIView):
     def get(self, request, slug):
         product = Product.objects.get(slug=slug)
         inital = {"items":[],"price":0.0,"count":0}
         session = request.session.get("data", inital)
         if slug in session["items"]:
-            messages.error(request, "Already added to cart")
+            return Response({"error": "Already added to cart"}, status=status.HTTP_400_BAD_REQUEST)
         else:
             session["items"].append(slug)
             session["price"] += float(product.price)
             session["count"] += 1
             request.session["data"] = session
-            messages.success(request, "Added successfully")
-        return redirect("shop:detail", slug)
+            return Response({"success": "Added successfully"}, status=status.HTTP_200_OK)
 
-
-class MyCartView(View):
+class MyCartView(APIView):
     def get(self, request):
         sess = request.session.get("data", {"items":[]})
         products = Product.objects.filter(active=True, slug__in=sess["items"])
-        categories = Category.objects.filter(active=True)
-        context = {"products": products,
-                   "categories": categories,
-                   "title": "My Cart"}
-        return render(request, "shop/list.html", context)
+        serializer = ProductSerializer(products, many=True)
+        data = {"products": serializer.data}
+        return Response(data, status=status.HTTP_200_OK)
 
-
-class CheckoutView(View):
+class CheckoutView(APIView):
     def get(self, request):
         request.session.pop('data', None)
-        return redirect("/")
-
-
+        return Response(status=status.HTTP_200_OK)
 
 class ApiProductsView(APIView):
     def get(self, request):
         query = request.GET.get("q", "")
         products = Product.objects.filter(Q(name__contains=query) | Q(description__contains=query))
         serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
