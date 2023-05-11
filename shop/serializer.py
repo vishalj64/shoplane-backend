9d7f5fe7a69212842b08aa5f17300e38f4cdccd2
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-05-11 15:59:02 +0530

    Update serializer.py

--- shop/serializer.py
+++ shop/serializer.py
@@ -6,3 +6,9 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('category', 'slug', 'name', 'image', "description")
+        
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'mobile_number', 'password']
