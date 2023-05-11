2cc5799afb4624976e79164557e57d2441984565
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-05-11 15:51:23 +0530

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -51,3 +51,15 @@ class Review(models.Model):
 
     def __str__(self):
         return self.review
+    
+    
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True)
+    mobile_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.review
