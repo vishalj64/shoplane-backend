81cbf2b4040533a4fa0f9c62b0c6d148540c4f67
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-05-31 04:07:48 +0530

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -62,3 +62,30 @@ class Review(models.Model):
 
     def __str__(self):
         return self.review
+    
+  from django.db import models
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=10)
+    order_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.order_number
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+
+class BillingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=10)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    orders = models.ManyToManyField(Order)
+
