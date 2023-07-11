745e82f531b1eaf801840679465a8e7ecc371afa
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-07-11 10:29:43 +0530

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,7 @@ from autoslug import AutoSlugField
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 
@@ -52,18 +53,40 @@ class Review(models.Model):
     def __str__(self):
         return self.review
 
- class User(models.Model):
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True)
-    email = models.EmailField(unique=True)
     mobile_number = models.CharField(max_length=20)
-    password = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'mobile_number']
+
+    objects = UserManager()
 
     def __str__(self):
-        return self.review
+        return self.email
+
     
-  from django.db import models
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
