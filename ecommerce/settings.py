93409d4a05f1e2bd50f5361c8d96449e08203662
Vishal Jumnani <vishalj64@users.noreply.github.com>
2023-07-12 10:54:16 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'ecommerce.middleware.JWTAuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
